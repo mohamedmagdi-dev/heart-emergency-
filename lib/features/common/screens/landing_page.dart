42fc1b29d456a7fe274a937d0ad550ecac9f600e
mohamed magdi <mohamedgata784@gmail.com>
2025-09-28 14:40:43 +0300

    refactor: Update landing page navigation
    This commit changes the navigation target from `/test` to `/login` when the user completes the final step of the onboarding process on the landing page.

--- lib/features/common/screens/landing_page.dart
+++ lib/features/common/screens/landing_page.dart
@@ -112,7 +112,7 @@ class _LandingPageState extends State<LandingPage> {
                   TextButton(
                     onPressed: () {
                       if (_currentPage == 5) {
-                        context.go('/test');
+                        context.go('/login');
                         return;
                       }
                       setState(() {
